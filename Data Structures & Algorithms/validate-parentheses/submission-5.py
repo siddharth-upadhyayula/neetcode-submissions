6752c49f05aa457c73f214396feaa0be980cf9c8
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2026-08-23 14:40:20 -0400

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        map = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+
+        for i in s:
+            if i in map:
+                if not stack or stack[-1]!=map[i]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(i)
+
+        return not stack

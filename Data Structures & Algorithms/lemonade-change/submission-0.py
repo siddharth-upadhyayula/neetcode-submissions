94f6e23a326b5b1efc1cbdf5e60377179dd203b8
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2026-09-17 19:28:54 -0400

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        map = {5:0,10:0,20:0}
+
+        for i in range(len(bills)):
+            if bills[i]==5:
+                map[bills[i]]+=1
+
+            if bills[i]==10:
+                if map[5]<1:
+                    return False
+                map[5]-=1
+                map[bills[i]]+=1
+                
+            if bills[i]==20:
+                if (map[5]<3 and map[10]<1) or (map[5]<1 and map[10]>1):
+                    return False
+                if map[10]:
+                    map[10]-=1
+                    map[5]-=1
+                else:
+                    map[5]-=3
+
+                if map[5]<0 or map[10]<0:
+                    return False
+
+        return True 
+
+            
